d1a0c81ea8622ffbb94d92634fa7f579dae8b15f
elkanoun <43721781+elkanoun@users.noreply.github.com>
2024-12-08 02:37:58 +0100

    Update ELearningApplication.java

--- e-learning/src/main/java/smartsofts/applicatif/elearning/ELearningApplication.java
+++ e-learning/src/main/java/smartsofts/applicatif/elearning/ELearningApplication.java
@@ -11,29 +11,7 @@ public class ELearningApplication {
 	public static void main(String[] args) {
 		ApplicationContext context=SpringApplication.run(ELearningApplication.class, args);
 		
-		//data students
-		/*
-		 * StudentRepository studentRepository=context.getBean(StudentRepository.class);
-		 * 
-		 * Student student=new Student(null, "KMED", "med.elkanoun@gmail.com",
-		 * "mohamed", "el-kanoun", "cheikh lmfddl salé", "0653627305", "isi", "isi10",
-		 * "essahel"); studentRepository.save(student);
-		 */
 		
-		//data school	
-		/*
-		SchoolRepository schoolRepository=context.getBean(SchoolRepository.class);
-		School school=new School(null, "supmti", 50, null);
-		schoolRepository.save(school);
-		
-		StudentRepository studentRepository=context.getBean(StudentRepository.class);
-		List<Student> students=studentRepository.findAll();
-		
-		for (Student student : students) {
-			student.setSchool(school);
-			studentRepository.save(student);
-		}
-		*/
 		
 		
 	}
